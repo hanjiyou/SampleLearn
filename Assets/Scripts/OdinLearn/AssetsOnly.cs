1df94e75ffa98070dd695bc1170f1292860d8f36
DESKTOP-PHKB03R\han <1156781974@qq.com>
2019-10-10 23:26:59 +0800

    Odin插件开始学习1

---
+++ Assets/Scripts/OdinLearn/AssetsOnly.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+/// <summary>
+/// 两种特性
+/// AssetsOnly特性对应的Obj，点击打开选择弹窗时，只有Project中的资源文件，不会出现Hierachy（场景）的资源
+/// SceneObjectsOnly:点击需要序列化的资源字段时，在出现的弹窗中只有Hierachy中的资源文件，不会出现Project中的资源（public默认）
+/// </summary>
+public class AssetsOnly : MonoBehaviour
+{
+    public GameObject AllGO;
+    
+    [AssetsOnly]
+    public List<GameObject> OnlyPrefabs;
+
+    [AssetsOnly]
+    public GameObject SomePrefab;
+
+    [AssetsOnly]
+    public Material MaterialAsset;
+
+    [AssetsOnly]
+    public MeshRenderer SomeMeshRendererOnPrefab;
+
+    [SceneObjectsOnly]
+    public List<GameObject> OnlySceneObjects;
+
+    [SceneObjectsOnly]
+    public GameObject SomeSceneObject;
+
+    [SceneObjectsOnly]
+    public MeshRenderer SomeMeshRenderer;
+}
---
+++ Assets/Scripts/OdinLearn/CustomValueDrawerExample.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEditor;
+using UnityEngine;
+/// <summary>
+/// CustomValueDrawer 自定义字段绘制方法 参数:绘制方法名
+/// </summary>
+public class CustomValueDrawerExample : MonoBehaviour
+{
+    public GameObject DefaultGO;
+    
+    [CustomValueDrawer("HaveLabelNameFunction")]
+    public string HaveLabelName;
+    [CustomValueDrawer("NoLabelNameFunction")]
+    public string NoLabelName;
+
+    public string HaveLabelNameFunction(string tempName, GUIContent label)
+    {
+        return EditorGUILayout.TextField(label,"韩"+tempName);
+    }
+    public string NoLabelNameFunction(string tempName, GUIContent label)
+    {
+        return EditorGUILayout.TextField(label,tempName);
+    }
+}
