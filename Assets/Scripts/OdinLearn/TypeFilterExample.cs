0d0ef1e642085bd5a27dfcc27f8e658e5e2acdba
hanjiyou <1156781974@qq.com>
2019-10-14 20:53:14 +0800

    odin TypeFilter特性和Valueinput特性

--- Assets/Scripts/OdinLearn/TypeFilterExample.cs
+++ Assets/Scripts/OdinLearn/TypeFilterExample.cs
@@ -1,8 +1,62 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Sirenix.OdinInspector;
 using UnityEngine;
-
+/// <summary>
+/// 类型过滤器案例
+/// </summary>
 public class TypeFilterExample : MonoBehaviour
 {
+    [ShowInInspector]
+    public BaseClass ANoFilter;
+
+    [ShowInInspector]
+    [TypeFilter("FilterGeneric")]
+    public BaseClass AFilterGeneric;
     
+    [ShowInInspector,TypeFilter("FilterSon")]
+    public BaseClass AFilterSon;
+    
+    [ShowInInspector]
+    public TestTypeSer C;
+
+    IEnumerable<Type> FilterGeneric()
+    {
+        var q = typeof(BaseClass).Assembly.GetTypes()
+            .Where(x=>x.IsGenericType);
+
+        return q;
+    }
+
+
+    IEnumerable<Type> FilterSon()
+    {
+        var q = typeof(BaseClass).Assembly.GetTypes()
+            .Where(x=>typeof(BaseClass).IsAssignableFrom(x))
+            .Where(x=>typeof(BaseClass)!=x);
+        return q;
+    }
+    
+    
+    
+    
+    
+    
+    public abstract class BaseClass
+    {
+        public int BaseField;
+    }
+    public class A1 : BaseClass { public int _A1; }
+    public class A2 : A1 { public int _A2; }
+    public class A3 : A2 { public int _A3; }
+    public class B1 : BaseClass { public int _B1; }
+    public class B2 : B1 { public int _B2; }
+    public class B3 : B2 { public int _B3; }
+    public class C1<T> : BaseClass { public T C; }
+}
+public class TestTypeSer
+{
+    public int Age;
 }
---
+++ Assets/Scripts/OdinLearn/ValueInputExample.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Sirenix.Utilities.Editor;
+using UnityEngine;
+/// <summary>
+/// 检查值特性
+/// </summary>
+public class ValueInputExample : MonoBehaviour
+{
+    [ReadOnly]
+    public string dynamicMessage = "这个物体不应该为空！";
+    [ValidateInput("CheckGameObject", "$dynamicMessage", InfoMessageType.None)]
+    public GameObject targetObj_None = null;
+    [ValidateInput("CheckGameObject", "$dynamicMessage", InfoMessageType.Info)]
+    public GameObject targetObj_Info = null;
+    [ValidateInput("CheckGameObject", "$dynamicMessage", InfoMessageType.Warning)]
+    public GameObject targetObj_Warning = null;
+    [ValidateInput("CheckGameObject", "$dynamicMessage", InfoMessageType.Error)]
+    public GameObject targetObj_Error = null;
+    /// <summary>
+    /// 检查函数 如果结果为false 则显示消息盒子
+    /// </summary>
+    /// <param name="tempObj"></param>
+    /// <returns></returns>
+    private bool CheckGameObject(GameObject tempObj)
+    {
+        return tempObj != null;
+    }
+    
+    [ValidateInput("HasMeshRendererDynamicMessage", "对应的函数中已经有消息，所以这个默认消息已经没用")]
+    public GameObject DynamicMessage;
+    private bool HasMeshRendererDynamicMessage(GameObject gameObject, ref string errorMessage)
+    {
+        if (gameObject == null) return true;
+
+        if (gameObject.GetComponentInChildren<MeshRenderer>() == null)
+        {
+            errorMessage = "\"" + gameObject.name + "\" 这玩应必须有一个 MeshRenderer 组件";//如果设置消息，则默认消息会被覆盖
+            return false;
+        }
+        return true;
+    }
+
+    [ValidateInput("HasMeshRendererDynamicMessageAndType", "对应的函数中已经有消息和类型，所以这个默认消息和类型已经没用")]
+    public GameObject DynamicMessageAndType;
+
+    [ShowInInspector]
+//    [OnValueChanged("OnInfoTypeChanged")]
+    [InfoBox("Change GameObject value to update message type", InfoMessageType.Info)]
+    public InfoMessageType MessageType { get; set; }
+    private bool HasMeshRendererDynamicMessageAndType(GameObject gameObject, ref string errorMessage, ref InfoMessageType? messageType)
+    {
+        if (gameObject == null) return true;
+
+        if (gameObject.GetComponentInChildren<MeshRenderer>() == null)
+        {
+            errorMessage = "\"" + gameObject.name + "\" 要有一个 MeshRenderer 组件";//如果设置消息，则默认消息会被覆盖
+            messageType = this.MessageType;//如果设置消息类型，则默认消息类型会被覆盖
+            return false;
+        }
+        return true;
+    }
+
+//    static void OnInfoTypeChanged()
+//    {
+//        GUIHelper.RequestRepaint();
+//    }
+//    
+    
+}
