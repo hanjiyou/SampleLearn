7a26b1242e05ca743b949be1dae8b463d8d8799e
hanjiyou <1156781974@qq.com>
2019-11-02 17:52:18 +0800

    ref关键字和协程执行完毕为空

---
+++ Assets/Scripts/Learn/TestCoroutine.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 协程执行完毕，协程对象也不会为空
+/// </summary>
+public class TestCoroutine : MonoBehaviour
+{
+    private Coroutine _coroutine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _coroutine=StartCoroutine(TestCor1());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_coroutine == null)
+        {
+            Loger.Log("hhh _coroutine为空");
+        }
+        else
+        {
+            Loger.Log("hhh _coroutine不");
+        }
+    }
+
+    IEnumerator TestCor1()
+    {
+        Loger.Log("hhh TestCor1 111");
+        yield return new WaitForSeconds(1);
+        Loger.Log("hhh TestCor1 2");
+    }
+}
---
+++ Assets/Scripts/Learn/TestRef.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//如果ref指定的实参为null,则对形参的分配和修改都会同步到ref指定的实参引用。
+//如果不指定ref，上述操作则不同步
+public class TestRef : MonoBehaviour
+{
+    private GameObject _gameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject goAsset = Resources.Load<GameObject>("HexagonlMap/Prefab/HexCellLabel");
+        TestRefAssignment(ref _gameObject, goAsset);
+        Loger.Log("1111111_gameObject.name="+_gameObject.name);
+        TestRefSetNull(ref _gameObject);
+        Loger.Log("22222_gameObject.name="+_gameObject.name);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TestRefAssignment(ref GameObject go,GameObject goAsset)
+    {
+        go = GameObject.Instantiate(goAsset);
+    }
+
+    void TestRefSetNull(ref GameObject go)
+    {
+        go = null;
+    }
+}
