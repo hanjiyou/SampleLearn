fc7abab193498c5c1bd3750c1889fe7cdfe9ef49
hanjiyou <hanjiyou123>
2020-05-27 14:13:28 +0800

    使用迭代器+计时器实现协程的分帧+延迟  嵌套协程嵌套延迟即可

---
+++ Assets/Scripts/Learn/LearnCoroutine/CoroutineExcute.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineExcute : MonoBehaviour
+{
+    private IEnumerator _enumerator;
+
+    private OnceTimer _onceTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    IEnumerator MyCor()
+    {
+        LogTool.Log($"hhh{Time.frameCount} 111");
+        yield return null;
+        LogTool.Log($"hhh{Time.frameCount} 222");
+        yield return new WaitForSeconds(3);
+        LogTool.Log($"hhh{Time.frameCount} 333");
+        yield return null;
+        LogTool.Log($"hhh{Time.frameCount} 444");
+    }
+
+    private void LateUpdate()
+    {
+        if (_enumerator != null)
+        {
+            bool needWait = _enumerator.Current is WaitForSeconds;
+            LogTool.Log("hhh moveNext之前");
+            if (needWait)
+            {
+                if (_onceTimer == null)
+                {
+                    _onceTimer=new OnceTimer(3);
+                }
+                else
+                {
+                    _onceTimer.OnUpdate();
+                     
+                }
+            }
+
+            if (needWait && !_onceTimer.OnUpdate())
+            {
+                return;
+            }
+            if (!_enumerator.MoveNext())
+            {
+                _enumerator = null;
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("btnaaaaaaaaaaaaaaaaaaaaa"))
+        {
+            _enumerator = MyCor();
+        }
+    }
+}
---
+++ Assets/Scripts/TimerTool/TimerTool.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Timer
+{
+    protected float _delayTimer;
+    protected float _delayTime;
+    protected bool _isOver;
+    protected bool _isPause;
+
+    protected Timer(float delayTime)
+    {
+        _delayTime = delayTime;
+    }
+    public bool OnUpdate()//这个在Mono的LateUpdate()中调用
+    {
+        if (_isOver)
+        {
+            return false;
+        }
+
+        if (_isOver)
+        {
+            return false;
+        }
+
+        _delayTimer += Time.deltaTime;
+        if (_delayTimer >= _delayTime)
+            return Update();
+        return false;
+    }
+
+    public void SetPause()
+    {
+        _isPause = true;
+    }
+
+    public void Recover()
+    {
+        _isPause = false;
+    }
+    public bool GetOver()
+    {
+        return _isOver;
+    }
+    public void Release()
+    {
+        _isOver = false;
+        _isPause = false;
+        _delayTime = 0;
+        _delayTimer = 0;
+    }
+    protected abstract bool Update();
+}
+
+public class OnceTimer : Timer
+{
+    public OnceTimer(float delayTime) : base(delayTime)
+    {
+        
+    }
+    protected override bool Update()
+    {
+        return true;
+    }
+}
+
+
+public class RepeatTimer : Timer
+{
+    private float _delayTime;
+    private int _repeatTime;
+    private int _repeatTimer;
+    public RepeatTimer(float delayTime,int repeatTime) : base(delayTime)
+    {
+        _delayTime = delayTime;
+        _repeatTime = repeatTime;
+    }
+    
+    protected override bool Update()
+    {
+        if (_repeatTimer < _repeatTime)
+        {
+            _delayTimer += Time.deltaTime;
+            if (_delayTimer >= _delayTime)
+            {
+                _delayTimer = 0;
+                _repeatTimer++;
+                return true;
+            }
+            return false;
+        }
+
+        _isOver = true;
+        return false;
+    }
+}
+
