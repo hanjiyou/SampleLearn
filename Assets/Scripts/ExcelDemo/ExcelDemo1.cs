37cb3267718100dabb429012b46bc8288374019c
hanjiyou <hanjiyou123>
2020-06-03 20:33:07 +0800

    ExcelReader初次使用

---
+++ Assets/Scripts/ExcelDemo/ExcelDemo1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class ExcelDemo1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DataSet dataSet= ExcelReader.ReadExcel("Hero/HeroCard.xlsx");
+        foreach (DataRow rows in dataSet.Tables[0].Rows)
+        {
+            string rowContent="";
+            foreach (DataColumn cols in dataSet.Tables[0].Columns)
+            {
+                rowContent += rows[cols].ToString()+" ";
+            }
+                LogTool.Log("hhh "+rowContent);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ExcelDemo/ExcelReader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using ExcelDataReader;
+using UnityEngine;
+
+public  class ExcelReader
+{
+    private static string excelRootPath = Application.dataPath+"/Editor/ExcelConvert/ExcelTable/";
+    public static DataSet ReadExcel(string excelName)
+    {
+        using (var stream=File.Open(excelRootPath+excelName,FileMode.Open,FileAccess.Read))
+        {
+            using (var reader=ExcelReaderFactory.CreateReader(stream))
+            {
+                DataSet result = reader.AsDataSet();
+                return result;
+            }
+        }
+    }
+}
